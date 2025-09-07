8fc984417cbbabd32518b3bcd276c6cab6d0cb07
Randika Lakmal <randika.l.abeyrathna@gmail.com>
2025-09-07 23:55:03 +0530

    init commit

---
+++ src/main/java/com/randikalakmal/spring_modulith_sample/SpringModulithSampleApplication.java
@@ -0,0 +1,13 @@
+package com.randikalakmal.spring_modulith_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulithSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModulithSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randikalakmal/spring_modulith_sample/SpringModulithSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randikalakmal.spring_modulith_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
